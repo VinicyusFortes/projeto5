12925e8f5acdb55c501b5e072e9eb1058efa4347
VinicyusFortes <vinicyusfortess@gmail.com>
2025-04-04 15:20:18 +0100

    retirado codigo lixo; update index.js: incluida logica para nao exibir header e footer nas páginas de login, regito e recuperacao de senha

--- frontend/react-app/src/index.js
+++ frontend/react-app/src/index.js
@@ -1,37 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Route, Routes } from "react-router-dom";
-import Footer from './Components/Footer';
-import Header from './Components/Header';
-import Login from './Pages/Login';
-import Home from './Pages/Home';
-import CreateAccount from './Pages/CreateAccount';
-import Profile from './Pages/Profile';
-import ForgotPassword from './Pages/ForgotPasword';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Login from "./Pages/Login";
+import Home from "./Pages/Home";
+import CreateAccount from "./Pages/CreateAccount";
+import Profile from "./Pages/Profile";
+import ForgotPassword from "./Pages/ForgotPasword";
 
+const App = () => {
+  const location = useLocation();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Router>
-      <Header />
+  // Define as rotas onde o Header/Footer NÃO devem aparecer
+  const hideLayoutPaths = ["/login", "/create-account", "/forgot-password"];
+
+  
+  const shouldHideLayout = hideLayoutPaths.includes(location.pathname);
+
+  return (
+    <>
+      {!shouldHideLayout && <Header />}
       <Routes>
         <Route index path="/home" element={<Home />} />
         <Route path="/create-account" element={<CreateAccount />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile/>} />
-        <Route path="/forgot-password" element={<ForgotPassword/>} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
-      <Footer />
+      {!shouldHideLayout && <Footer />}
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <App />
     </Router>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
